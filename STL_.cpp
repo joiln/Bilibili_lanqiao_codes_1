0658682c5e8ed4c141148fbd6b4423bd1e971145
joiln <29aw@sina.cn>
2019-08-17 16:47:12 +0800

    LanQiaoCup_daily_practice

--- STL_.cpp
+++ STL_.cpp
@@ -2,12 +2,12 @@
 // Created by chen on 2019/8/17 16:35.
 //
 
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
 
-    \t
+    cout << "Hello world" << endl;
     return 0;
 }
 
